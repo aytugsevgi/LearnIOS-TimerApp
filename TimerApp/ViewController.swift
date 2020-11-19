92e1074c6266c132ac07b3af31954017872a7bb3
aytug.sevgi <aytug3667@gmail.com>
2020-11-19 17:17:46 +0300

    first commit

--- TimerApp/ViewController.swift
+++ TimerApp/ViewController.swift
@@ -9,12 +9,58 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    var timer = Timer()
+    var counter = 10
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        timeLabel.text = "Time: \(counter)"
+        stopButton.isHidden = true
+    }
+    
+    @objc func timerFunc(){
+        counter -= 1
+        
+        if counter == 0 {
+            timer.invalidate()
+            timeLabel.text = "Time is Over"
+            changeButton(isEnable: true)
+            stopButton.isHidden = true
+        } else {
+            timeLabel.text = "Time: \(counter)"
+        }
+        
     }
 
-
+    @IBAction func buttonClicked(_ sender: Any) {
+        stopButton.isHidden = false
+        changeButton(isEnable: false)
+        
+        counter = 10
+        timeLabel.text = "Time: \(counter)"
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerFunc), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func stopButtonClicked(_ sender: Any) {
+        timer.invalidate()
+        changeButton(isEnable: true)
+        stopButton.isHidden = true
+        timeLabel.text = "Time: \(counter)"
+    }
+    
+    func changeButton(isEnable : Bool!){
+        if isEnable {
+            button.setTitle("Start Timer", for: UIControl.State.normal)
+            button.isEnabled = true
+        } else{
+            button.setTitle("Timer Running...", for: UIControl.State.normal)
+            button.isEnabled = false
+        }
+    }
+    
 }
 
